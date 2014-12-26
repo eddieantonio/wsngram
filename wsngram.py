e96500be215866b44001ee23a8d7c243b1739b55
Eddie Antonio Santos <easantos@ualberta.ca>
2014-12-26 08:35:32 -0700

    A whole bunch of weird stuff...

--- wsngram.py
+++ wsngram.py
@@ -2,13 +2,13 @@
 # coding: utf-8
 
 # Copyright 2014 Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,22 +40,24 @@ STRATEGIES = {
     'order': (1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
     'interleaved': (True,),
     'tweengrams': (0,),
-    'use_text': (True, False),
+    'key': ('text', 'category'),
     'smoothing': (False,),
     #'interleaved': (True, False),
     #'tweengrams': (0, 1),
     #'smoothing': (True, False),
 }
 
+# TODO: logical line is wrong EVERYWHERE.
+
 TOKEN_FIELDS = 'type text start end line'
 
-ALWAYS_CATEGORY = (token.NEWLINE, token.INDENT, token.DEDENT, tokenize.NL,
-        token.ERRORTOKEN, token.ENDMARKER)
+ALWAYS_USE_CATEGORY = (token.NEWLINE, token.INDENT, token.DEDENT, tokenize.NL,
+                       token.ERRORTOKEN, token.ENDMARKER)
 
-CATEGORY     = 0
-TEXT         = 1
-START        = 2
-END          = 3
+CATEGORY = 0
+TEXT = 1
+START = 2
+END = 3
 LOGICAL_LINE = 4
 
 ROW = 0
@@ -70,23 +72,54 @@ Start = namedtuple('Start', TOKEN_FIELDS)('START',
                                           1)
 Whitespace = namedtuple('Whitespace', TOKEN_FIELDS)
 
-def whitespace_tokenize(source_lines):
+
+def make_whitespace(text, start, end=None, logical_line=None):
+    if logical_line is None:
+        logical_line = ''
+    if end is None:
+        end = find_end(text, start)
+    return Whitespace('WHITESPACE', text, start, end, logical_line)
+
+
+def find_end(text, start):
+    lines_down = text.count('\n')
+    end_col = len(text) - 1
+    if '\n' in text:
+        end_col -= text.rfind('\n')
+    else:
+        end_col += start[COLUMN]
+    return (start[ROW] + lines_down, end_col)
+
+
+def tokenize_with_whitespace(source_lines):
+    r"""
+    Tokenizes and with interleaved whitespace tokens.
+
+    >>> lines = ['#!/usr/bin/env\n', '\n', 'import os\n', '\n',
+    ...     'for i in range(5):\n', '    print(i)\n']
+    >>> tokens = tokenize_with_whitespace(lines)
+    >>> all(isinstance(token, Whitespace) for token in tokens[::2])
+    True
+    >>> any(isinstance(token, Whitespace) for token in tokens[1::2])
+    False
+
+    """
     lineiter = iter(source_lines)
     readline = lambda: next(lineiter)
 
     # Create a list of tokens WITHOUT
     tokens_sans_nl = (token for token in tokenize.generate_tokens(readline)
                       if token[0] != tokenize.NL)
-
     tokens_with_start = itertools.chain((Start,), tokens_sans_nl)
 
     def generate():
         # Generate the whitespace and the token. Since the very first token in
-        # the sequnce is START, it gets silently thrown out.
+        # the sequence is START, it gets silently thrown out.
         for prev_token, token in bigrams(tokens_with_start):
-            yield gen_whitespace(prev_token, token, source_lines)
+            yield whitespace_between(prev_token, token, source_lines)
             yield token
     return list(generate())
+
 
 def bigrams(sequence):
     """
@@ -103,8 +136,7 @@ def bigrams(sequence):
     return itertools.izip(normal, peek)
 
 
-
-def gen_whitespace(token, next_token, source_lines):
+def whitespace_between(token, next_token, source_lines):
     # Start is end of token.
     srow, scol = token[END]
     start = (srow, scol + 1)
@@ -118,38 +150,58 @@ def gen_whitespace(token, next_token, source_lines):
         text = line[start[COLUMN]:end[COLUMN]]
     else:
         lines_between = erow - srow
-        end_line = source_lines[erow - 1] if (erow - 1) < len(source_lines) else ''
+        end_line = source_lines[
+            erow - 1] if (erow - 1) < len(source_lines) else ''
         text = '\n' * lines_between + end_line[:ecol]
 
-    return Whitespace('WHITESPACE', text, start, end, token[LOGICAL_LINE])
+    return make_whitespace(text, start, logical_line=token[LOGICAL_LINE])
+
 
 def use_text(token):
-    if token[0] in ALWAYS_CATEGORY:
+    if token[0] in ALWAYS_USE_CATEGORY:
         return '<%s>' % (canonical_category_name(token),)
     return token[1]
 
-def use_category(token):
-    return (token.tok_name.get(token[0], token[0]),)
 
-def canonical_category_name(token):
-    "Returns the 'canonical' name of the token's category."
-    return token.tok_name.get(token[1], token[1])
+def use_category(token):
+    return canonical_category_name(token)
+
+
+def canonical_category_name(tok):
+    """
+    Returns the 'canonical' name of the token's category.
+
+    >>> canonical_category_name((1, 'for', (1, 1), (1, 3), 1))
+    'NAME'
+    >>> canonical_category_name(make_whitespace(' ', (1, 1)))
+    'WHITESPACE'
+    """
+    return token.tok_name.get(tok[0], tok[0])
+
 
 def interleave_ws(sequence, order):
     """
     n-grams interleaving whitespace tokens between normal tokens.
 
-    >>> sequence = whitespace_tokenize(['if False:'])
-    >>> map(type, next(iter(interleave_ws(sequence, 3))))
-    [<class 'wsngram.Whitespace'>, <type 'tuple'>, <class 'wsngram.Whitespace'>]
+    >>> sequence = tokenize_with_whitespace(['if False:'])
+    >>> ngram = list(interleave_ws(sequence, 6))[0]
+    >>> [use_category(token) for token in ngram]
+    ['NAME', 'WHITESPACE', 'NAME', 'WHITESPACE', 'OP', 'WHITESPACE']
     """
 
-    # Skip every... thing.
-    for i in xrange(0, len(sequence) - order, 2):
+    # We assume that the sequence consists of interleaved whitespace and
+    # language tokens. The sequence should start with a whitespace tokens.
+    # Then, ngrams with an even order will need to skip the first token in
+    # order to always end in a whitespace token.
+    offset = 1 if (order % 2) == 0 else 0
+
+    # Skip every... whitespace...
+    for i in xrange(offset, len(sequence) - order, 2):
         ngram = sequence[i:i + order]
         assert type(ngram[-1]) is Whitespace
         assert len(ngram) == order
         yield ngram
+
 
 def ws_last(sequence, order):
     """
@@ -169,6 +221,7 @@ def ws_last(sequence, order):
         assert len(ngram) == order
         yield ngram
 
+
 def tween(ngram, t=1):
     """
     >>> tween([1, 2, 3, 4, 5])
@@ -185,30 +238,46 @@ def tween(ngram, t=1):
 
 def simplify(token, store_text_func=use_category):
     """
-    Simplifies the token.
+    Simplifies the token for storage in the model.
     """
 
     if isinstance(token, Whitespace):
         return ('W', token.text)
     return ('T', store_text_func(token))
 
-def make_ngrams(sequence, order=None, interleaved=None, tweengrams=None, use_text=None):
+
+def unsimplify(token, line=1, col=1):
+    """
+    >>> unsimplify(('T', '<HERP>'))
+    (-1, '<HERP>', (1, 1), (1, 6), 1)
+    >>> unsimplify(('W', ' '), 1, 7)
+    Whitespace(type='WHITESPACE', text=' ', start=(1, 7), end=(1, 7), line='')
+    """
+    tag, text = token
+    if tag == 'T':
+        return (-1, text, (line, col), find_end(text, (line, col)), line)
+    elif tag == 'W':
+        return make_whitespace(text, (line, col))
+    else:
+        raise ValueError("Invalid token: %r" % (token,))
+
+
+def unsimplify_str(token):
+    return unsimplify((token[0], token[1:]))
+
+
+def make_ngrammer(interleaved=None, key='text', **kwargs):
     """
     Creates n-grams.... very carefully!
 
     Assumes the input is already interleaved with whitespace tokens.
     """
 
-    # Pad the start with START tokens.
-    #start_padding = [Start] * (order - 1)
-    #full_sequence = list(itertools.chain(start_padding, sequence))
-
-    # The ngramming thingy...
     ngrammer = interleave_ws if interleaved else ws_last
-    store_text = get_text if use_text else use_category
+    store_text = use_text if key == 'text' else use_category
 
-    for ngram in ngrammer(full_sequence, order):
-        if tweengrams:
-            ngram = tween(ngram, tweengrams)
-        yield [simplify(token, store_text) for token in ngram]
+    def make_ngrams(seqeunce, order):
+        for ngram in ngrammer(sequence, order):
+            yield [simplify(token, store_text) for token in ngram]
 
+    return make_ngrams
